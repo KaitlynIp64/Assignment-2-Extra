466595cd51a6a869ba2e5d2459b30118a2b4d7e8
KaitlynIp64 <99354960+KaitlynIp64@users.noreply.github.com>
2022-03-28 20:13:49 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,28 @@
+// Created by: Kaitlyn Ip
+// Created on: Mar 2022
+//
+// This program finds the perimeter of a right triangle.
+
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // This function accepts user input
+        int Base;
+        int Height;
+
+        Console.WriteLine("This program finds the income of the user.");
+        Console.WriteLine("");
+        Console.Write("Enter the base: ");
+        Base = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter the height: ");
+        Height = Convert.ToInt32(Console.ReadLine());
+        Double perimeter = Base + Height + ((Base ** 2 + Height ** 2) ** 0.5);
+        Console.WriteLine("");
+        Console.Write("Perimeter: " + perimeter.ToString("0.00") + "cm");
+        Console.ReadKey();
+        Console.WriteLine("\nDone.");
+    }
+}
