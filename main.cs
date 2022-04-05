c41efedeafcc849b2b59039faeeea9361e3d4f7a
KaitlynIp64 <99354960+KaitlynIp64@users.noreply.github.com>
2022-04-05 19:49:22 +0000

    fixed code

--- main.cs
+++ main.cs
@@ -10,16 +10,16 @@ class Program
     public static void Main(string[] args)
     {
         // This function accepts user input
-        int Base;
-        int Height;
+        int length;
+        int height;
 
-        Console.WriteLine("This program finds the income of the user.");
+        Console.WriteLine("This program finds the perimeter of a right triange.");
         Console.WriteLine("");
         Console.Write("Enter the base: ");
-        Base = Convert.ToInt32(Console.ReadLine());
+        length = Convert.ToInt32(Console.ReadLine());
         Console.Write("Enter the height: ");
-        Height = Convert.ToInt32(Console.ReadLine());
-        Double perimeter = Base + Height + (Math.Pow((Math.Pow(Base, 2) + Math.Pow(Height, 2)), 0.5));
+        height = Convert.ToInt32(Console.ReadLine());
+        Double perimeter = length + height + (Math.Pow((Math.Pow(length, 2) + Math.Pow(height, 2)), 0.5));
         Console.WriteLine("");
         Console.Write("Perimeter: " + perimeter.ToString("0.00") + "cm");
         Console.ReadKey();
